86da3e1f88c644e4b496dbfd74b95e0677acd7e8
victoriaaa333 <yb244@cornell.edu>
2024-03-13 18:31:18 -0400

    small sample correction

--- simulations/test_for_efficiency(marginal).R
+++ simulations/test_for_efficiency(marginal).R
@@ -4,7 +4,37 @@ source("estimator_implementation/WCLS_original.R")
 source("estimator_implementation/GEE_estimators.R")
 source("estimator_implementation/improve_efficiency.R")
 
-data_generating_process <- dgm_binary_categorical_covariate_new
+compute_result_beta <- function(beta_true, beta, beta_se, beta_se_adjusted, moderator_vars, control_vars, significance_level,
+                                na.rm = FALSE) {
+  
+  beta_true_array <- array(NA, dim = dim(beta), dimnames = dimnames(beta))
+  for (ind1 in 1:dim(beta_true_array)[1]) {
+    for (ind3 in 1:dim(beta_true_array)[3]) {
+      beta_true_array[ind1, , ind3] <- beta_true
+    }
+  }
+  
+  p <- length(moderator_vars) + 1
+  q <- length(control_vars) + 1
+  
+  bias <- apply(beta - beta_true_array, c(1,2), mean, na.rm = na.rm)
+  sd <- apply(beta, c(1,2), sd, na.rm = na.rm)
+  rmse <- apply(beta - beta_true_array, c(1,2), function(v) sqrt(mean(v^2, na.rm = na.rm)))
+  
+  critical_factor <- qnorm(1 - significance_level/2)
+  ci_left <- beta - critical_factor * beta_se
+  ci_right <- beta + critical_factor * beta_se
+  coverage_prob <- apply((ci_left < beta_true_array) & (ci_right > beta_true_array),
+                         c(1,2), mean, na.rm = na.rm)
+  
+  critical_factor_adj <- qt(1 - significance_level/2, df = sample_size - 1 - q)
+  ci_left_adj <- beta - critical_factor_adj * beta_se_adjusted
+  ci_right_adj <- beta + critical_factor_adj * beta_se_adjusted
+  coverage_prob_adj <- apply((ci_left_adj < beta_true_array) & (ci_right_adj > beta_true_array),
+                             c(1,2), mean, na.rm = na.rm)
+  
+  return(list(bias = bias, sd = sd, rmse = rmse, coverage_prob = coverage_prob, coverage_prob_adjusted = coverage_prob_adj))
+}
 
 library(tidyverse)
 library(foreach)
@@ -14,9 +44,10 @@ library(doRNG)
 max_cores <- 16
 registerDoMC(min(detectCores() - 1, max_cores))
 sample_sizes <- c(30, 50, 100)
-nsim <- 100
+nsim <- 1000
 
-Delta <- 3 # Delta <- 10
+data_generating_process <- dgm_binary_categorical_covariate_new
+Delta <- 10 # Delta <- 10
 total_T <- 100 
 beta_true_marginal <- beta_true_marginal_generalDelta(Delta)
 
@@ -98,8 +129,10 @@ for (i_ss in 1:length(sample_sizes)) {
                                                            nrow = length(ee_names), byrow = TRUE, dimnames = list(ee_names, beta_names))))
   beta_se <- simplify2array(lapply(result, function(l) matrix(c(l$fit_wcls_improved$beta_se, l$fit_wcls_new$beta_se, l$fit_wcls$beta_se),
                                                               nrow = length(ee_names), byrow = TRUE, dimnames = list(ee_names, beta_names))))
+  beta_se_adjusted <- simplify2array(lapply(result, function(l) matrix(c(l$fit_wcls_improved$beta_se_adjusted, l$fit_wcls_new$beta_se_adjusted, l$fit_wcls$beta_se_adjusted),
+                                                                       nrow = length(ee_names), byrow = TRUE, dimnames = list(ee_names, beta_names))))
   
-  # result <- compute_result_beta_woadjusted(beta_true_marginal, beta, beta_se, moderator_vars, control_vars, significance_level, na.rm = FALSE)
+  
   result <- compute_result_beta(beta_true_marginal, beta, beta_se, beta_se_adjusted, moderator_vars, control_vars, significance_level = 0.05)
   
   result_df <- data.frame(ss = rep(sample_size, num_estimator),
@@ -107,20 +140,12 @@ for (i_ss in 1:length(sample_sizes)) {
                           bias = result$bias,
                           sd = result$sd,
                           rmse = result$rmse,
-                          cp.unadj = result$coverage_prob)
-  names(result_df) <- c("ss", "est", "bias", "sd", "rmse", "cp.unadj")
+                          cp.unadj = result$coverage_prob,
+                          cp.adj = result$coverage_prob_adjusted)
+  names(result_df) <- c("ss", "est", "bias", "sd", "rmse", "cp.unadj", "cp.adj")
   rownames(result_df) <- NULL
   
   result_df_collected <- rbind(result_df_collected, result_df)
   
 }
-saveRDS(result_df_collected, file = "result_simulation_improved(marginal, Delta = 10).RDS")
-
-result_df_collected <- result_df_collected[, c(2, 1, 3:ncol(result_df_collected))]
-result_df_collected$est <- factor(result_df_collected$est, c("improved.EMEE", "pd.EMEE", "EMEE"))
-result_df_collected <- result_df_collected[order(result_df_collected$est, result_df_collected$ss), ]
-result_df_collected[-1] <- round(result_df_collected[-1],3)
-
-rownames(result_df_collected) <- NULL
-colnames(result_df_collected) <- 
-  c("Estimator", "Sample Size","Bias","SD","RMSE","CP(unadj)")
+saveRDS(result_df_collected, file = "result_simulation_improved_delta10_marginal.RDS")
--- simulations/test_for_efficiency.R
+++ simulations/test_for_efficiency.R
@@ -4,7 +4,37 @@ source("estimator_implementation/WCLS_original.R")
 source("estimator_implementation/GEE_estimators.R")
 source("estimator_implementation/improve_efficiency.R")
 
-data_generating_process <- dgm_binary_categorical_covariate_new
+compute_result_beta <- function(beta_true, beta, beta_se, beta_se_adjusted, moderator_vars, control_vars, significance_level,
+                                na.rm = FALSE) {
+  
+  beta_true_array <- array(NA, dim = dim(beta), dimnames = dimnames(beta))
+  for (ind1 in 1:dim(beta_true_array)[1]) {
+    for (ind3 in 1:dim(beta_true_array)[3]) {
+      beta_true_array[ind1, , ind3] <- beta_true
+    }
+  }
+  
+  p <- length(moderator_vars) + 1
+  q <- length(control_vars) + 1
+  
+  bias <- apply(beta - beta_true_array, c(1,2), mean, na.rm = na.rm)
+  sd <- apply(beta, c(1,2), sd, na.rm = na.rm)
+  rmse <- apply(beta - beta_true_array, c(1,2), function(v) sqrt(mean(v^2, na.rm = na.rm)))
+  
+  critical_factor <- qnorm(1 - significance_level/2)
+  ci_left <- beta - critical_factor * beta_se
+  ci_right <- beta + critical_factor * beta_se
+  coverage_prob <- apply((ci_left < beta_true_array) & (ci_right > beta_true_array),
+                         c(1,2), mean, na.rm = na.rm)
+  
+  critical_factor_adj <- qt(1 - significance_level/2, df = sample_size - 1 - q)
+  ci_left_adj <- beta - critical_factor_adj * beta_se_adjusted
+  ci_right_adj <- beta + critical_factor_adj * beta_se_adjusted
+  coverage_prob_adj <- apply((ci_left_adj < beta_true_array) & (ci_right_adj > beta_true_array),
+                             c(1,2), mean, na.rm = na.rm)
+  
+  return(list(bias = bias, sd = sd, rmse = rmse, coverage_prob = coverage_prob, coverage_prob_adjusted = coverage_prob_adj))
+}
 
 library(tidyverse)
 library(foreach)
@@ -14,11 +44,12 @@ library(doRNG)
 max_cores <- 16
 registerDoMC(min(detectCores() - 2, max_cores))
 sample_sizes <- c(30, 50, 100)
-nsim <- 100
+nsim <- 1000
 
-Delta <- 3 # Delta <- 10
+data_generating_process <- dgm_binary_categorical_covariate_new
+Delta <- 10
 total_T <- 100 
-beta_true_marginal <- c(0.1, 0.2)
+beta_true <- c(0.1, 0.2)
 
 control_vars <- "S"
 moderator_vars <- "S"
@@ -102,8 +133,7 @@ for (i_ss in 1:length(sample_sizes)) {
                                                               nrow = length(ee_names), byrow = TRUE, dimnames = list(ee_names, beta_names))))
   
   
-  # result <- compute_result_beta_woadjusted(beta_true, beta, beta_se, moderator_vars, control_vars, significance_level = 0.05, na.rm = FALSE)
-  result <- compute_result_beta(beta_true_marginal, beta, beta_se, beta_se_adjusted, moderator_vars, control_vars, significance_level = 0.05)
+  result <- compute_result_beta(beta_true, beta, beta_se, beta_se_adjusted, moderator_vars, control_vars, significance_level = 0.05)
   
   result_df <- data.frame(ss = rep(sample_size, num_estimator),
                           est = ee_names,
@@ -118,5 +148,5 @@ for (i_ss in 1:length(sample_sizes)) {
   result_df_collected <- rbind(result_df_collected, result_df)
 }
 
-saveRDS(result_df_collected, file = "result_simulation_improved(Delta = 3, ss adjust).RDS")
+saveRDS(result_df_collected, file = "result_simulation_improved_delta10_moderator.RDS")
 
